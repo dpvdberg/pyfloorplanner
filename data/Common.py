d6b8da8616a6e72d3da496df0ad1e1ec4a7dce00
Dennis van den Berg <d.p.v.d.berg@student.tue.nl>
2021-03-10 19:59:40 +0100

    Add Floorplan visualization

--- data/Common.py
+++ data/Common.py
@@ -2,12 +2,6 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Dimensions:
-    width: float
-    height: float
-
-
-@dataclass
 class Vector2:
     x: float
     y: float
@@ -15,6 +9,42 @@ class Vector2:
     @staticmethod
     def from_tuple(t: (float, float)):
         return Vector2(t[0], t[1])
+
+    def to_tuple(self) -> (float, float):
+        return self.x, self.y
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector2(self.x * other, self.y * other)
+        else:
+            raise ValueError("Multiplication with type {} not supported".format(type(other)))
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector2(self.x / other, self.y / other)
+        else:
+            raise ValueError("Division with type {} not supported".format(type(other)))
+
+    def __iter__(self):
+        return iter([self.x, self.y])
+
+
+@dataclass
+class Dimensions:
+    width: float
+    height: float
+
+    def to_vector(self) -> Vector2:
+        return Vector2(self.width, self.height)
 
 
 @dataclass
--- data/Floorplan.py
+++ data/Floorplan.py
@@ -1,4 +1,12 @@
+import math
+
+import matplotlib
+import numpy as np
+from matplotlib.collections import PatchCollection
+
 from data.Module import *
+from matplotlib import pyplot as plt, cm
+from matplotlib.patches import Rectangle
 
 
 class Floorplan:
@@ -7,14 +15,51 @@ class Floorplan:
         self.aspect_ratio: Interval = self.compute_aspect_ratio_interval()
 
     def compute_aspect_ratio_interval(self) -> Interval:
-        min_ar = sys.float_info.max
-        max_ar = sys.float_info.min
+        min_ar = math.inf
+        max_ar = -math.inf
         for module in self.modules:
             ar = module.dimensions.height / module.dimensions.width
             min_ar = min(min_ar, ar)
             max_ar = max(max_ar, ar)
 
         return Interval(min_ar, max_ar)
+
+    def max_dimension(self):
+        max_dimension = 0
+        for m in self.modules:
+            max_dimension = max(max_dimension, m.position.x + m.dimensions.width)
+            max_dimension = max(max_dimension, m.position.y + m.dimensions.height)
+
+        return max_dimension
+
+    def plot(self, draw_names=False, name_size=6):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, aspect='equal')
+        patches = []
+        for i, m in enumerate(self.modules):
+            r = Rectangle(m.position.to_tuple(), m.dimensions.width, m.dimensions.height)
+            patches.append(r)
+
+            if draw_names:
+                ax.annotate(m.name, m.position + 0.5 * m.dimensions.to_vector(), color='w', weight='bold',
+                            fontsize=name_size, ha='center', va='center')
+
+        max_dimension = self.max_dimension()
+        plt.xlim([0, max_dimension])
+        plt.ylim([0, max_dimension])
+
+        pc = PatchCollection(patches, cmap=cm.get_cmap('viridis'), edgecolor='black')
+        # assign random values for cmap coloring
+        pc.set_array(np.random.random(len(self.modules)))
+
+        ax.add_collection(pc)
+        plt.show()
+
+    def align_horizontally(self):
+        x = 0
+        for m in self.modules:
+            m.position.x = x
+            x += m.dimensions.width
 
     def __str__(self):
         return "Floorplan: \n" + "\n".join([str(m) for m in self.modules])
